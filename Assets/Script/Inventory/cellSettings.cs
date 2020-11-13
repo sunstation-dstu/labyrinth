2835baae9018dff7a5e2a3de9a126651f3573ec7
TheGrids <72039184+TheGrids@users.noreply.github.com>
2020-11-13 18:11:29 +0300

    Reworking inventory code |  Fix bugs with item selection
    Я немного переделал систему инвентаря и привёл код более ли менее в порядок (наверняка Юра сделал бы лучше, но да ладно)
    UI инвентаря я вынес в отдельный скрипт, теперь нет того нагромождения данных в одном скрипте.
    Теперь иконки остальных предметов инвентаря сгруппированы сеткой, что позволяет легко добавить или убрать новые ячейки.
    Теперь новые ячейки добавлять очень просто, главное в дочернем объекте дать название этой ячейке следующей цифрой предыдущей (т.е. было пять ячеек, вы клонируете ещё одну и даёте название "6"). Скрипт сам определяет её, а сетка правильно подстраивает её положение
    Избавился от костылей по типу заднего бэкграунда для выделения активной ячейки. Теперь просто понижается прозрачность
    Добавил в инспектор "энумы" (выпадающие списки), избавившись от тупого костыля с переменными isgun и istool.
    В общем, всё стало куда удобнее, чем было

---
+++ Assets/Script/Inventory/cellSettings.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class cellSettings : MonoBehaviour
+{
+    [HideInInspector]
+    public bool active = false;
+    public int iD;
+    public enum typeItem
+    {
+        gun = 0,
+        tool = 1,
+        other = 2
+    }
+    public typeItem type;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (active == true) gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 0.7f);
+        else gameObject.GetComponent<Image>().color = new Color(1, 1, 1, 0.4f);
+    }
+}
