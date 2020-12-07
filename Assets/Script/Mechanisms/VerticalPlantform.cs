f70c71f3f0a3808d25e9c7d34e567a2f5843bd4b
TheDoctorDiscord <73133119+TheDoctorDiscord@users.noreply.github.com>
2020-12-07 17:56:55 +0300

    Реализация односторонних платформ

---
+++ Assets/Script/Mechanisms/VerticalPlantform.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalPlantform : MonoBehaviour
+{
+    private PlatformEffector2D effector;
+    public float waitTime; 
+
+    void Start()
+    {
+        effector = GetComponent<PlatformEffector2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            waitTime = 0.1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+   
+            if(waitTime <= 0)
+            {
+                effector.rotationalOffset = 180f;
+                waitTime = 0.1f;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            effector.rotationalOffset = 0;
+        }
+    }
+}
